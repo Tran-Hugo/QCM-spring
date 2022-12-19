2f656c7e86f558e31d271441e86d9fa2fa7c7b8c
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-12-19 11:26:05 +0100

    models

---
+++ src/main/java/com/qcm/qcm/models/User.java
@@ -0,0 +1,94 @@
+package com.qcm.qcm.models;
+
+import java.util.List;
+import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToMany(targetEntity=Score.class,mappedBy = "score")
+    private List<Score> scores;
+
+    public User() {
+    }
+
+    public User(int id, String name, List<Score> scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Score> getScores() {
+        return this.scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
+
+    public User id(int id) {
+        setId(id);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public User scores(List<Score> scores) {
+        setScores(scores);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(scores, user.scores);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, scores);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", scores='" + getScores() + "'" +
+            "}";
+    }
+
+
+}
