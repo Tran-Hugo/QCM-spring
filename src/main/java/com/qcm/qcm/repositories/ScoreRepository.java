bf337f629c8d93a46fbff91139df2b146eb29b73
Anisse <anissbouainbi@hotmail.it>
2022-12-22 16:40:51 +0100

    ajout route score

--- src/main/java/com/qcm/qcm/repositories/ScoreRepository.java
+++ src/main/java/com/qcm/qcm/repositories/ScoreRepository.java
@@ -1,10 +1,13 @@
 package com.qcm.qcm.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.qcm.qcm.models.Qcm;
 import com.qcm.qcm.models.Score;
+import com.qcm.qcm.models.User;
 
 public interface ScoreRepository extends JpaRepository<Score, Integer>{
     Score findByQcm(Qcm qcm);
+    Score findByQcmAndUser(Qcm qcm, User user);
 }
